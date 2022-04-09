8131dff4487ce7d3b57eb48940837ef50bbb5306
bemuc <muciroben@gmail.com>
2022-04-09 20:59:09 +0200

    habuzi vrmnt

--- gestionclient/urls.py
+++ gestionclient/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home,name='home'),
-    path('login/', views.loginPage,name='login_page'),
+    path('', views.loginPage,name='login_page'),
+    path('home/', views.home,name='home'),
     path('logout/', views.logout_page,name='logout'),
 
 
