1695efb08f9bcfbb9895ef24aef31015c7056984
bemuc <muciroben@gmail.com>
2022-04-10 12:29:39 +0200

    enfin resolu

--- gestion/settings.py
+++ gestion/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'gestionclient.views.afacturer',
+                'gestionclient.views.afacturer',
                 # 'gestionclient.views.home',
             ],
         },
